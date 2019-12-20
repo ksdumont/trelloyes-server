d5d6fa4f53f31735582fc89068e432fd8673ba61
ksdumont <ksdumont@gmail.com>
2019-12-20 08:52:02 -0800

    list folder added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
 const uuid = require('uuid/v4');
 const cardRouter = require('./card/card-router');
+const listRouter = require('./list/list-router')
 
 const app = express()
 
@@ -29,74 +30,11 @@ app.use(function validateBearerToken(req, res, next) {
 })
 
 app.use(cardRouter);
+app.use(listRouter);
 
-
-app.get('/list', (req, res) => {
-    res.json(lists);
-});
-
-   
-
-app.get('/list/:id', (req, res) => {
-    const {id} = req.params;
-    const list = lists.find(li => li.id == id);
-
-    if (!list) {
-        logger.error(`List with id ${id} not found.`);
-        return res.status(404).send('List Not Found');
-    }
-    res.json(list);
-});
-
-// POST
-
-app.post('/list', (req, res) => {
-    const {header, cardIds = []} = req.body;
-
-    if (!header) {
-        logger.error(`Header is required`);
-        return res.status(400).send('Invalid data');
-    }
-    if (cardIds.length > 0) {
-        let valid = true;
-        cardIds.forEach(cid => {
-            const card = cards.find(c => c.id == cid);
-            if (!card) {
-                logger.error(`Card with id ${cid} not found in cards array.`);
-                valid = false;
-            }
-        });
-        if (!valid) {
-            return res.status(400).send('Invalid data');
-        }
-    }
-    const id = uuid();
-    const list = {
-        id, 
-        header, 
-        cardIds
-    };
-    lists.push(list);
-    logger.info(`List with id ${id} created`);
-    res.status(201).location(`http://localhost:8000/list/${id}`).json({id});
-});
-
-// Delete
-
-app.delete('/list/:id', (req, res) => {
-    const {id} = req.params;
-    const listIndex = lists.findIndex(li => li.id == id);
-
-    if (listIndex === -1) {
-        logger.error(`List with id ${id} not found.`);
-        return res.status(404).send('Not Found');
-    }
-    lists.splice(listIndex, 1);
-
-    logger.info(`List with id ${id} deleted.`);
-    res.status(204).end();
+app.get('/', (req, res) => {
+    res.send('Hello, world')
 })
-
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
